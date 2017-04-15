9adb49b824907149c6130f55aaf95a9c56b7043b
Kong Ka Weng <mgongzai@gmail.com>
2017-04-15 19:08:04 +0800

    lesson - 4

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,10 @@ module.exports = {
     ]
   },
   /*
+  ** Global css
+  */
+  css: ['tachyons/css/tachyons.css'],
+  /*
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
